0cdb5f84fa56545c336a6caa91e250c46a7069fc
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 11:33:12 +0100

    Make Ruby examples Protocol 2.0 compatible

--- software/examples/ruby/example_simple.rb
+++ software/examples/ruby/example_simple.rb
@@ -10,10 +10,11 @@ HOST = 'localhost'
 PORT = 4223
 UID = '7oj' # Change to your UID
 
-ipcon = IPConnection.new HOST, PORT # Create IP connection to brickd
-ai = BrickletAnalogIn.new UID # Create device object
-ipcon.add_device ai # Add device to IP connection
-# Don't use device before it is added to a connection
+ipcon = IPConnection.new # Create IP connection
+ai = BrickletAnalogIn.new UID, ipcon # Create device object
+
+ipcon.connect HOST, PORT # Connect to brickd
+# Don't use device before ipcon is connected
 
 # Get current voltage (unit is mV)
 voltage = ai.get_voltage / 1000.0
@@ -21,4 +22,3 @@ puts "Voltage: #{voltage} V"
 
 puts 'Press key to exit'
 $stdin.gets
-ipcon.destroy
