167780af41b30fae87dc2e40ec07358bd0753171
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 15:36:33 +0100

    Add Perl examples

---
+++ software/examples/perl/example_callback.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletAnalogIn;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'abd2'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $ai = BrickletAnalogIn->new(&UID, $ipcon); # Create device object
+
+# Callback function for voltage callback (parameter has unit mV)
+sub cb_voltage
+{
+    my ($voltage) = @_;
+    print"\nVoltage: ".$voltage/1000.0." V\n";
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Set Period for voltage callback to 1s (1000ms)
+# Note: The callback is only called every second if the 
+#       voltage has changed since the last call!
+$ai->set_voltage_callback_period(1000);
+
+# Register voltage callback to function cb_voltage
+$ai->register_callback($ai->CALLBACK_VOLTAGE, 'cb_voltage');
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
---
+++ software/examples/perl/example_simple.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletAnalogIn;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'abd2'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $ai = BrickletAnalogIn->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Get current voltage (unit is mV)
+my $voltage = $ai->get_voltage();
+
+print "\nVoltage: ".$voltage/1000.0." V\n";
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
