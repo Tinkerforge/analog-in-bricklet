b0a6df941efa2cd1599ca5ba6fb2d2489fbe1ed5
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 17:22:02 +0100

    perl: fixed namespace

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -7,8 +7,8 @@ use constant HOST => 'localhost';
 use constant PORT => 4223;
 use constant UID => 'abd2'; # Change to your UID
 
-my $ipcon = IPConnection->new(); # Create IP connection
-my $ai = BrickletAnalogIn->new(&UID, $ipcon); # Create device object
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $ai = Tinkerforge::BrickletAnalogIn->new(&UID, $ipcon); # Create device object
 
 # Callback function for voltage callback (parameter has unit mV)
 sub cb_voltage
