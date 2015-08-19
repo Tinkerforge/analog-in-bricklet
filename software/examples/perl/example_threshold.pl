65bee7cf98473af1155263a65b675d686bb44a82
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:37:48 +0200

    Cleanup Perl examples

--- software/examples/perl/example_threshold.pl
+++ software/examples/perl/example_threshold.pl
@@ -1,33 +1,33 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletAnalogIn;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'abd2'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $ai = Tinkerforge::BrickletAnalogIn->new(&UID, $ipcon); # Create device object
 
-# Callback for voltage smaller than 5V
-sub cb_reached
+# Callback subroutine for voltage smaller than 5 V (parameter has unit mV)
+sub cb_voltage_reached
 {
     my ($voltage) = @_;
 
-    print "Voltage dropped below 5V: ".$voltage/1000.0." \n";
+    print "Voltage: " . $voltage/1000.0 . " V\n";
 }
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Get threshold callbacks with a debounce time of 10 seconds (10000ms)
-$ai->set_debounce_period(1000);
+$ai->set_debounce_period(10000);
 
-# Register threshold reached callback to function cb_reached
-$ai->register_callback($ai->CALLBACK_VOLTAGE_REACHED, 'cb_reached');
+# Register threshold reached callback to subroutine cb_voltage_reached
+$ai->register_callback($ai->CALLBACK_VOLTAGE_REACHED, 'cb_voltage_reached');
 
-# Configure threshold for "smaller than 5V" (unit is mV)
+# Configure threshold for "smaller than 5 V" (unit is mV)
 $ai->set_voltage_callback_threshold('<', 5*1000, 0);
 
 print "Press any key to exit...\n";
