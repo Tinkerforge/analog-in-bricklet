77803ae92ac37281a78058c9d00ff7b2eb46932c
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:18:39 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,31 +1,32 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{analog_in_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{analog_in_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let analog_in_bricklet = AnalogInBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ai = AnalogInBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for voltage events.
-    let voltage_listener = analog_in_bricklet.get_voltage_receiver();
-    // Spawn thread to handle received events. This thread ends when the analog_in_bricklet
+    // Create receiver for voltage events.
+    let voltage_receiver = ai.get_voltage_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `ai` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in voltage_listener {
-            println!("Voltage: {}{}", event as f32 / 1000.0, " V");
+        for voltage in voltage_receiver {
+            println!("Voltage: {} V", voltage as f32 / 1000.0);
         }
     });
 
-    // Set period for voltage listener to 1s (1000ms)
+    // Set period for voltage receiver to 1s (1000ms).
     // Note: The voltage callback is only called every second
     //       if the voltage has changed since the last call!
-    analog_in_bricklet.set_voltage_callback_period(1000);
+    ai.set_voltage_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,32 +1,33 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{analog_in_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{analog_in_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let analog_in_bricklet = AnalogInBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ai = AnalogInBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get threshold listeners with a debounce time of 10 seconds (10000ms)
-    analog_in_bricklet.set_debounce_period(10000);
+    // Get threshold receivers with a debounce time of 10 seconds (10000ms).
+    ai.set_debounce_period(10000);
 
-    //Create listener for voltage reached events.
-    let voltage_reached_listener = analog_in_bricklet.get_voltage_reached_receiver();
-    // Spawn thread to handle received events. This thread ends when the analog_in_bricklet
+    // Create receiver for voltage reached events.
+    let voltage_reached_receiver = ai.get_voltage_reached_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `ai` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in voltage_reached_listener {
-            println!("Voltage: {}{}", event as f32 / 1000.0, " V");
+        for voltage_reached in voltage_reached_receiver {
+            println!("Voltage: {} V", voltage_reached as f32 / 1000.0);
         }
     });
 
-    // Configure threshold for voltage "smaller than 5 V"
-    analog_in_bricklet.set_voltage_callback_threshold('<', 5 * 1000, 0);
+    // Configure threshold for voltage "smaller than 5 V".
+    ai.set_voltage_callback_threshold('<', 5 * 1000, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
